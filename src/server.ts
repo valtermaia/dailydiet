4693c5d5cbba915321950e83280b223bee195e9d
Valter Maia <valtermaia@tjro.jus.br>
2024-09-03 14:25:54 -0400

    config env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import { app } from './app'
+import { env } from './env'
+
+import { usersRoute } from './routes/users.route'
+
+app.register(usersRoute, {
+  prefix: 'users',
+})
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log(`HTTP Server Running on port 3333! 🚀`)
+    console.log(`HTTP Server Running on port ${env.PORT}! 🚀`)
   })
