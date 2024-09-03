e7953b218468541ff11c1f7715f31d8e99c031ba
Valter Maia <valtermaia@tjro.jus.br>
2024-09-03 13:22:20 -0400

    create database and migrations

---
+++ db/migrations/20240903164648_create-users-table.ts
@@ -0,0 +1,14 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('username').notNullable()
+    table.text('email').notNullable().unique()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
